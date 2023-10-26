6504563d78bd48b17bc0122233c2d7be8b5ce695
Administrator <elishevaronstam@gmail.com>
2023-10-26 10:21:27 +0300

    done rest api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express, {Application, Request, Response} from 'express';
 import dotenv from 'dotenv';
-import { v4 as uuidv4 } from 'uuid';
-
-import {getInitTasks} from "./helper";
-import Task from "./Types/task";
+import {createTaskFromJson, getInitTasks} from './helper'
+import Task from "./types/task";
 
 //For env File
 dotenv.config();
@@ -33,59 +31,12 @@ app.get('/tasks', async (req: Request, res: Response) => {
 });
 
 app.post("/create", async (req: Request, res: Response) => {
-    // let validBody;
-    // if(req.body.status === "Close")
-    //     validBody=closedTaskValidation(req.body);
-    // else if(req.body.priority === "High")
-    //     validBody=highPriorityTaskValidation(req.body);
-    // else
-    //     validBody=generalTaskValidation(req.body);
-    //
-    // if (validBody.error) {
-    //     res.status(400).json(validBody.error.details)
-    // }
+
     try {
-        let newTask: Task;
-        if (req.body.status === "Close") {
-
-            newTask = {
-                id: uuidv4(),
-                title: req.body.title,
-                description: req.body.description,
-                estimatedTime: req.body.estimatedTime,
-                status: req.body.status,
-                priority: req.body.priority,
-                review: req.body.review,
-                timeSpent: req.body.timeSpent,
-                endTime: req.body.endTime ? req.body.endTime : undefined,
-            };
-        } else if (req.body.priority === "High") {
-            newTask = {
-                id: uuidv4(),
-                title: req.body.title,
-                description: req.body.description,
-                estimatedTime: req.body.estimatedTime,
-                status: req.body.status,
-                priority: req.body.priority,
-                endTime: req.body.endTime ? req.body.endTime : undefined,
-            };
-
-        } else {
-            newTask = {
-                id: uuidv4(),
-                title: req.body.title,
-                description: req.body.description,
-                estimatedTime: req.body.estimatedTime,
-                status: req.body.status,
-                priority: req.body.priority,
-            };
-
-
-        }
+        let newTask: Task = createTaskFromJson(req.body);
         tasksObjects.push(newTask);
         res.json(newTask);
     } catch (err) {
-        console.log(err)
         res.status(500).json({msg: "err in post task", err})
     }
 })
@@ -95,12 +46,36 @@ app.delete('/delete/:taskId', (req: Request, res: Response) => {
     const taskIndex = tasksObjects.findIndex((task) => task.id === taskId);
 
     if (taskIndex === -1) {
-        return res.status(404).json({ message: 'Task not found' });
+        return res.status(404).json({message: 'Task not found'});
     }
 
     tasksObjects.splice(taskIndex, 1);
 
-    return res.status(200).json({ message: 'Task deleted' });
+    return res.status(200).json({message: 'Task deleted'});
+});
+
+app.get('/filter', (req: Request, res: Response) => {
+    try {
+        const filterByOpenStatus: boolean = req.query.param1 === 'true'; // Convert to boolean
+        const filterByHighPriority: boolean = req.query.param2 === 'true'; // Convert to boolean
+        const keyword: string = req.query.param3.toString();
+
+        let filteredTasks = tasksObjects;
+        if (keyword != "") {
+            filteredTasks = filteredTasks.filter((task: Task) => {
+                return task.title.toString().toLowerCase().includes((keyword.toLowerCase()));
+            });
+        }
+        if (filterByOpenStatus) {
+            filteredTasks = filteredTasks.filter((task) => task.status === "Open");
+        }
+        if (filterByHighPriority) {
+            filteredTasks = filteredTasks.filter((task) => task.priority === "High");
+        }
+        res.json(filteredTasks);
+    } catch (err) {
+        res.status(500).json({msg: "err in filtering tasks", err})
+    }
 });
 
 
